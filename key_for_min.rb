b76a790654f1dae06f080895bcf215007d400211
John Stassen <john.stassen@gmail.com>
2020-08-22 18:20:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   key_for_lowest_value = 0
   name_hash.each do |key, value|
     if lowest_value == 0
+      lowest_value = value
       key_for_lowest_value = key
     elsif value < lowest_value
       lowest_value = value
